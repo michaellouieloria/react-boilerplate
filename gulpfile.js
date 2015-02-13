f76012670923e571fbf48f5ebab08fd3d7ca272c
Michael Louie Loria <michaellouieloria@gmail.com>
2015-02-13 11:18:27 +0800

    minify css and js

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,39 @@ var source = require('vinyl-source-stream');
 var connect = require('gulp-connect');
 var open = require('gulp-open');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglifyjs');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
+var cssmin = require('gulp-cssmin');
+var newer = require('gulp-newer');
 var port = process.env.port || 3000;
 
 gulp.task('sass', function() {
     gulp.src('./app/src/scss/**/*.scss')
         .pipe(sass())
+        .pipe(newer('./app/dist/css'))
+        .pipe(gulp.dest('./app/dist/css'));
+});
+
+gulp.task('sassprod', function() {
+    gulp.src('./app/src/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(cssmin())
+        .pipe(newer('./app/dist'))
         .pipe(gulp.dest('./app/dist'));
+});
+
+gulp.task('css', function() {
+    gulp.src('./app/src/css/**/*.css')
+        .pipe(newer('./app/dist/css'))
+        .pipe(gulp.dest('./app/dist/css'));
+});
+
+gulp.task('cssprod', function() {
+    gulp.src('./app/src/scss/**/*.css')
+        .pipe(cssmin())
+        .pipe(newer('./app/dist/css'))
+        .pipe(gulp.dest('./app/dis/css'));
 });
 
 gulp.task('browserify', function() {
@@ -17,7 +44,21 @@ gulp.task('browserify', function() {
     .transform('reactify', { 'es6': true })
     .bundle()
     .pipe(source('app.js'))
-    .pipe(gulp.dest('./app/dist'));
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .pipe(gulp.dest('./app/dist/js'));
+});
+
+gulp.task('browserifyprod', function() {
+    browserify('./app/src/js/app.js')
+    .transform('reactify', { 'es6': true })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .pipe(gulp.dest('./app/dist/js'));
 });
 
 gulp.task('connect', function() {
@@ -56,4 +97,5 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', ['browserify', 'sass']);
+gulp.task('production', ['browserifyprod', 'sassprod']);
 gulp.task('serve', ['browserify', 'sass', 'connect', 'open', 'watch']);
